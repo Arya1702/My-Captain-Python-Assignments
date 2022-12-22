34e8f04a0dde4c6a749ac662b84c8ba87575b366
Arya <120192495+Arya1702@users.noreply.github.com>
2022-12-22 23:28:33 +0530

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -1,9 +1,11 @@
-#1
+#1 Python program which accepts the radius of a circle from the user and computes area
+
 from math import pi
 r = float(input('Input the radius of the circle:'))
 print('The area of the circle with radius',r,'is:',pi*r**2)
 
-#2
+#2 Python program to accept a filename from the user and print the extension of that
+
 filename = input('Input the Filename:')
 f = filename.split(".")
 print ('The extension of the file is:', f[-1])
